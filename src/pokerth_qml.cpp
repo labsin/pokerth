c457891316986cb149015cc92df4b6fa97afb996
Sam Segers <sam.sgrs@gmail.com>
2014-05-15 11:18:57 +0200

    Temp hack

--- src/pokerth_qml.cpp
+++ src/pokerth_qml.cpp
@@ -1,4 +1,5 @@
-#include <QApplication>
+#include <QGuiApplication>
+#include <QQuickView>
 #include <QQmlContext>
 #include <QQmlComponent>
 #include <QDir>
@@ -6,7 +7,6 @@
 #include <QQuickWindow>
 #include <QDebug>
 #include "src/gui/qml/customqmlapplicationengine.h"
-#include "third_party/qtsingleapplication/qtsingleapplication.h"
 #include "configfile.h"
 #include "log.h"
 #include "gui/qml/manager.h"
@@ -18,11 +18,7 @@
 int main(int argc, char *argv[])
 {
     /////// can be removed for non-qt-guis ////////////
-    SharedTools::QtSingleApplication app( "PokerTH", argc, argv );
-
-    if (app.sendMessage("Wake up!")) {
-        return 0;
-    }
+    QGuiApplication app( argc, argv );
 
 #ifdef __APPLE__
     // The following needs to be done directly after the application is created.
@@ -33,12 +29,27 @@ int main(int argc, char *argv[])
 #endif
 
     //create defaultconfig
-    ConfigFile *myConfig = new ConfigFile(argv[0], false);
+    ConfigFile *myConfig = new ConfigFile(argv[0], true);
     Log *myLog = new Log(myConfig);
+
+    qDebug() << (myConfig->readConfigString("AppDataDir")).c_str();
 
     ManagerSingleton::Instance()->Init(myConfig, myLog);
 //    QString rscPath = QDir::currentPath() + "/data";
+    QUrl url("qrc:///qml/Main.qml");
 
+    QQuickView view;
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    QQmlEngine* engine = view.engine();
+    QString rscPath = ("file://"+myConfig->readConfigString("AppDataDir")).c_str();
+    engine->rootContext()->setContextProperty("rscPath",rscPath);
+    engine->rootContext()->setContextProperty("Manager",ManagerSingleton::Instance());
+    engine->rootContext()->setContextProperty("CurrentGame",ManagerSingleton::Instance()->getGame());
+    engine->rootContext()->setContextProperty("CurrentServer",ManagerSingleton::Instance()->getServer());
+    engine->addImageProvider("PokerTH",new ImageProvider);
+    view.setSource(url);
+    view.show();
+/*
     CustomQmlApplicationEngine engine;
     QObject::connect(&engine,SIGNAL(destroyed()), &app, SLOT(quit()));
     QString rscPath = ("file://"+myConfig->readConfigString("AppDataDir")).c_str();
@@ -47,17 +58,13 @@ int main(int argc, char *argv[])
     engine.rootContext()->setContextProperty("CurrentGame",ManagerSingleton::Instance()->getGame());
     engine.rootContext()->setContextProperty("CurrentServer",ManagerSingleton::Instance()->getServer());
     engine.addImageProvider("PokerTH",new ImageProvider);
-#ifdef NO_RESOURCES
-    QUrl url = QUrl::fromLocalFile("resources/qml/Main.qml");
-#else
-    QUrl url("qrc:///qml/Main.qml");
-#endif
 //    QQmlComponent component(&engine, url);
 //    QObject *object = component.create();
 //    if(object) {
 //        QMetaObject::invokeMethod(object,"show");
 //    }
     engine.load(url);
+*/
 
     int returnCode = app.exec();
 
