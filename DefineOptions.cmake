63024018c981f504c38df27571ba41f79c198055
Sam Segers <sam.sgrs@gmail.com>
2014-07-14 18:56:02 +0200

    update cmake

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,11 +1,12 @@
 include(CMakeDependentOption)
 option(QML_APP "Build with QML app" ON)
-option(UBUNTU_COMPONENTS "Use Ubuntu qml components" ON)
+option(ANDROID "Build for Android (todo)" OFF)
+option(UBUNTU_COMPONENTS "Use Ubuntu qml components" OFF)
 option(GUI_800x480 "Build 800x480 gui version" OFF)
 option(ONLY_CLIENT "Only build the client and not the server" ON)
 if(NOT APPLE)
     CMAKE_DEPENDENT_OPTION(BUILD_SERVER "Build the server" ON
-        "NOT ONLY_CLIENT;NOT GUI_800x480" OFF)
+        "NOT ONLY_CLIENT;NOT GUI_800x480;NOT ANDROID" OFF)
 elseif(NOT APPLE)
     CMAKE_DEPENDENT_OPTION(BUILD_SERVER "Build the server" OFF
         "NOT ONLY_CLIENT;NOT GUI_800x480" OFF)
