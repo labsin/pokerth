0f14e7d8b79e19f91aa7b37bbfdf5a9868ec39c0
Sam Segers <sam.sgrs@gmail.com>
2014-07-30 22:07:19 +0200

    Fix wrong qmlRegisterType for QmlEnums Wrong name

--- src/gui/qml/qmlenums.cpp
+++ src/gui/qml/qmlenums.cpp
@@ -6,5 +6,5 @@ QmlEnums::QmlEnums(QObject *parent) : QObject(parent){}
 
 void QmlEnums::registerType()
 {
-    qmlRegisterType<QmlEnums>("PokerTH",1, 0, "PlayerSortModel");
+    qmlRegisterType<QmlEnums>("PokerTH",1, 0, "Enums");
 }
